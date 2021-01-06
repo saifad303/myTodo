26edb766bed0a89d71753f1043f1d359589e4a20
Saif Ahmed <saifad303@gmail.com>
2021-01-06 21:23:41 +0600

    Pushing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import CounterHook from "./CounterHook";
 
 function App() {
   return (
     <div className="App">
-      
+      <CounterHook title={"my counter app"}/>
     </div>
   );
 }
