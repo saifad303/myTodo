b90620ac399e6587e80e95c1b0144d9e3fd94552
Saif Ahmed <saifad303@gmail.com>
2021-01-07 19:02:02 +0600

    adding another counter app

--- src/CounterHook.js
+++ src/CounterHook.js
@@ -1,9 +1,11 @@
 import React, {useState, useContext} from "react";
+import CountHookTwo from "./CountHookTwo";
 import { CreateContext } from "./App";
 
 function CounterHook({title}){
     const [counter, setCounter] = useState(0);
     const value = useContext(CreateContext);
+    const [visibility, setVisibility] = useState(true);
 
     const increase = () =>{
         setCounter(counter + 1);
@@ -12,6 +14,11 @@ function CounterHook({title}){
     const decrease = () =>{
         setCounter(counter - 1);
     }
+
+    const visibilityHandler = () =>{
+        setVisibility(!visibility);
+    }
+
     return (
         <>
             <div>
@@ -19,6 +26,10 @@ function CounterHook({title}){
                 <button style={value} onClick={increase}>+</button>
                 <h3>{counter}</h3>
                 <button style={value} onClick={decrease}>-</button>
+                <br/>
+                <button onClick={visibilityHandler}>Counter 2</button>
+                <hr/>
+                { visibility ? <CountHookTwo /> : null }
             </div>
         </>
     )
